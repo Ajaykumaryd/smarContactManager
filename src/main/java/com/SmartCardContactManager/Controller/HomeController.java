f1cfb8b91ccc5e35ec5991b0ad664fc86eeffdbf
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-09-06 23:08:39 +0530

    new changes added

--- src/main/java/com/SmartCardContactManager/Controller/HomeController.java
+++ src/main/java/com/SmartCardContactManager/Controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.SmartCardContactManager.Controller;
 
-import ch.qos.logback.core.model.Model;
+
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-   @RequestMapping("/homes")
-    public String home(Model model){
-//       model.addText("title", "Home");
-       model.add
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Smart Contact Manager");
         return "home";
     }
 
+    @RequestMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
+    }
 
+    @RequestMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("title", "Register - Smart Contact Manager");
+        return "signup";
+    }
 
 }
